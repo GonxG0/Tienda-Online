ee32901549943d998d1878ee65eeacfc738b846e
Gonzalo <gonzalo.digangi@gmail.com>
2024-06-07 08:22:56 -0300

    Last Commit

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="gonzalo.digangi@gmail.com"
-EMAIL_HOST_PASSWORD="yuaw jebl ftwb sgma"
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
